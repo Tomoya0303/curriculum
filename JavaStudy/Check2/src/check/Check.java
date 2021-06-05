6df0457ff1d96e4fc5d934749d2a083f505ac0e0
Tomoya0303 <niiniiniigata@gmail.com>
2021-06-06 06:37:59 +0900

    修正後

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -1,24 +1,23 @@
 package check;
 import constants.Constants;
+
 public class Check {
-	private static String firstName = "小林";
+	private static String firstName = "小林" ;
 	private static String lastName = "友哉";
-	private static String  name = Constants.CHECK_CLASS_JAVA;
-	private static String masterName = Constants.CHECK_CLASS_HOGE;
 	
-	private static void printName() {
-	System.out.println("printNameメソッド → " + firstName + lastName);
+	
+	private static void printName(String f,String l){
+	System.out.println("printNameメソッド → " + f + l );
 	}
-	public static void main(String[] args) {
-	printName();
 	
-	Pet pet = new Pet(name,masterName);
+	public static void main(String[] args) {
+	printName(firstName, lastName);
+	
+	
+	Pet pet = new Pet(Constants.CHECK_CLASS_JAVA,Constants.CHECK_CLASS_HOGE);
 	pet.introduce();
 	
-	name = Constants.CHECK_CLASS_R2D2;
-	masterName = Constants.CHECK_CLASS_LUKE;
-	RobotPet robo = new RobotPet(name,masterName);
+	RobotPet robo = new RobotPet(Constants.CHECK_CLASS_R2D2,Constants.CHECK_CLASS_LUKE);
 	robo.introduce();
 	}
-
 }
